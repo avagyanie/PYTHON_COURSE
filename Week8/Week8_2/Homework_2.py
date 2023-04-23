38779a3b66ba066fd84f634b7c835d5e2419c535
avagyanie <avagyannie@gmail.com>
2023-04-24 01:42:50 +0400

    changes

--- Week8/Week8_2/Homework_2.py
+++ Week8/Week8_2/Homework_2.py
@@ -9,11 +9,11 @@ Decorators, function inside function
 
 
 def func(lst):
-    return sorted(lst, key = lambda x: x[1][-1])
+    return sorted(lst, key = lambda x: x[1][-1], reverse=True)
         
-print(func(lst=[(19542209, "New York") ,
-                (4887871, "Alabama"), 
-                (1420491, "Hawaii"), 
-                (626299, "Vermont"), 
-                (1805832, "West Virginia"), 
-                (39865590, "California")]))
+print(func([(19542209, "New York") ,
+            (4887871, "Alabama"), 
+            (1420491, "Hawaii"), 
+            (626299, "Vermont"), 
+            (1805832, "West Virginia"), 
+            (39865590, "California")]))
