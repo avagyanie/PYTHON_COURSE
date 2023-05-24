556cb7cbd8a5e956e7871569255ada7fb61e2677
avagyanie <avagyannie@gmail.com>
2023-05-24 23:57:07 +0400

    changes

--- Week12/Week12_1/2.py
+++ Week12/Week12_1/2.py
@@ -4,15 +4,16 @@
 def outer(func):
     def inner(*args):
         res = func(*args)
-        return res
+        x = set()    #Յուրաքանչյուր տիպը 1 անգամ տպելու համար, եթե պետք է ամեն անգամ տպել, կարելի է լիստ սահմանել
+        for i in args:
+            x.add(type(i))
+        return x
     return inner
 
 @outer
 def new_func(*args):
-    x = set()    #Յուրաքանչյուր տիպը 1 անգամ տպելու համար, եթե պետք է ամեն անգամ տպել, կարելի է լիստ սահմանել
-    for i in args:
-        x.add(type(i))
-    return x
+    
+    return "Hello"
 
 a = new_func(2, "anun", 3.12, [1, 2, 3], (1, 1, 1), {4, 5, 6}, {"a": 1, "b": 2}, 120)
 
