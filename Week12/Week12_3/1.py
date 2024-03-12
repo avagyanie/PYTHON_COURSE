f5f641dacb4590c87066c16ba7c8f181b9317cee
avagyanie <130802015+avagyanie@users.noreply.github.com>
2024-03-12 21:39:25 +0400

    Update 1.py

--- Week12/Week12_3/1.py
+++ Week12/Week12_3/1.py
@@ -2,10 +2,15 @@
 Write a map function that adds plus 5 to each item in the list.
 """
 
-
+#version_1
 def plus_5(num):
     return num + 5
 
 lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 result = list(map(plus_5, lst))
+print(result)
+
+#version_2
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+result = list(map(lambda x: x + 5, lst))
 print(result)
